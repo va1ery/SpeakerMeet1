11a52c1c2897043495d43a4064ee6f6f444c6c84
Valery Gurov <va1ery@ya.ru>
2021-01-14 15:03:55 +0300

    first commit

---
+++ SpeakerMeet.API.Tests/SpeakerControllerSearchTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+using SpeakerMeet.API.Controllers; //<-- directive to add
+namespace SpeakerMeet.API.Tests
+{
+public class SpeakerControllerSearchTests //<-- to rename
+{
+[Fact]
+public void ItExists()
+{
+var controller = new SpeakerController();
+}
+}
+}
